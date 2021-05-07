3b6f465e9dff3182d09b6561930ba7ba442cc769
normidar <normidar7@gmail.com>
2021-05-07 13:14:13 +0900

    create

---
+++ main.py
@@ -0,0 +1,49 @@
+from html_builder.Body.Heading import Heading
+from html_builder.Body.Anchor import Anchor
+from html_builder.Body.Image import Image
+from html_builder.Body.Text import Text
+from html_builder.Html import Html
+from bs4 import BeautifulSoup
+import sys
+
+
+def test_1():
+    html = Html('abc')
+    html.body.addElement(Heading(1).text('标<题'))\
+        .addElement(Text('asdfasdf'))
+    return write(html)
+
+
+def test_2():
+    html = Html('abc')
+    body = html.body
+    body.addElement(Anchor('google.com').text('ググって'))
+    return write(html)
+
+
+def write(html):
+    print(html)
+    soup = BeautifulSoup(str(html), 'html.parser')
+    html = soup.prettify()
+    with open('index.html', 'w') as f:
+        f.write(html)
+    return html
+
+
+def main(params: list):
+    if len(params) == 3:
+        if params[1] == '-t':
+            if params[2] == '1':
+                return test_1()
+            if params[2] == '2':
+                return test_2()
+    elif len(params) == 1:
+        return test_1()
+    return '命令不存在'
+
+
+if __name__ == '__main__':
+    params = sys.argv
+    print(main(params))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
